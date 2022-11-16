8ba5e5046a2868f2530d7a5cc772f481da3abf17
DashingAdi <60130665+DashingAdi@users.noreply.github.com>
2022-11-16 20:13:06 +0530

    Update test.py
    Fixed syntax errors

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 # create our own module in a text editor
 
 # variables to import later
-length = 5
-width = 10
 
 # functions to import later 
 def printInfo(name, age):
-	print("{} is {} years old.".format(name, age))
-	
-	
+	print(f"{name} is {age} years old.")
+
+#initializing variable
+age=int(input())
+name=input()
+printInfo(name, age)	
 	
